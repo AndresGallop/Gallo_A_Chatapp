f7a76dfde40d83e9c24e8dd4b9ff487b0bf8e139
AndresGallop <andresgallod.i@gmail.com>
2021-02-09 18:01:39 -0500

    Sending_messages_function done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,28 @@
+import ChatMessage from "./components/TheMessageComponent.js";
 (() => {
     console.log('fired');
 
     // load the socket library and make a connection
     const socket = io();
 
+    // messenger service event handling -> incoming from the manager
+    function setUserId({sID, message}) {
+        // incoming connected event with data
+        // debugger;  
+        vm.socketID = sID;
+    }
+
+    function appendMessage(message) {
+        vm.messages.push(message);
+    }
+
     const vm = new Vue({
         data: {
-            messages: [],
+            messages: [], //these are the messages received
             nickname: "",
             username: "",
+            socketID: "",
+            message: ""   //these are the messages sent
         },
 
         created: function() {
@@ -16,7 +30,19 @@
         },
 
         methods: {
+            dispatchMassage() {
+                //debugger;
+                socket.emit('chatmessage', { content: this.message, name: this.nickname || "Anonymus" });
 
+                this.message = "";
+            }
+        },
+
+        components: {
+            newmessage: ChatMessage
         }
-    }).$mount("#app")
+    }).$mount("#app");
+
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener('message', appendMessage);
 })();
